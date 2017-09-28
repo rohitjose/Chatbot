fc29083f4976f61bd8d5fc67b16e964124a32dd9
alihaiderkhan <alihaiderkhan@gmail.com>
2017-09-28 14:49:26 +1000

    removed unused code from requestController

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -3,21 +3,15 @@ module.exports = {
 
     let intent = req.param('intentName');
     let params = req.param('parameters');
-
     let url = 'courseinformation/'+intent+'?parameters='+params;
 
-  console.log(url);
-    if (intent == 'course_description')
-      return res.redirect(url);
-
-    if (intent == 'class_detail')
-      return res.redirect('classdetails/get');
-
-    else {
+    if (!intent || !params) {
       return res.badRequest({
-        status: 'error',
-        msg:'intent could not be identified'
+        status:'error',
+        msg:'invalid arguments'
       });
     }
+    console.log(url);
+    return res.redirect(url);
   }
 }
