9db85b5210a24ac94cc4328ef1f40cfce44218f1
Rohit Jose <rohitjose@gmail.com>
2017-10-06 00:38:55 +1100

    Removed response encoding

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -32,7 +32,7 @@ module.exports = {
     http.get(options, function(resp) {
       console.log('STATUS: ' + res.statusCode);
       console.log('HEADERS: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
+      
       res.on('data', function(chunk) {
         console.log('BODY: ' + chunk);
       });
