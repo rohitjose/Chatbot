f82d04b33e76ce3b4430a1f6facbda0a47c03a13
Rohit Jose <rohitjose@gmail.com>
2017-10-06 00:25:01 +1100

    HTTP test

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var http = require('http');
 
 module.exports = {
   get: function(req, res) {
@@ -23,17 +23,19 @@ module.exports = {
     console.log("################ URL REDIRECT ###################");
     // return res.redirect(url);
 
-    request('http://localhost:1337' + url, function(error, response, body) {
-      console.log('error:', error); 
-      console.log('statusCode:', response && response.statusCode); 
-      console.log('body:', body); // Print the HTML for the Google homepage.
-    });
+    var options = {
+      host: 'localhost',
+      port: 1337,
+      path: url
+    };
 
-    // return res.ok({
-    //   "status": "success",
-    //   "displayText": "Description for COMP9323 e-Enterprise Project :\nCareer: postgraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\nDescription for COMP9323 e-Enterprise Project :\nCareer: undergraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\n",
-    //   "speech": "Description for COMP9323 e-Enterprise Project :\nCareer: postgraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\nDescription for COMP9323 e-Enterprise Project :\nCareer: undergraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\n",
-    //   "source": "http://www.handbook.unsw.edu.au/postgraduate/courses/2017/COMP9323.html"
-    // });
+    http.post(options, function(resp) {
+      resp.on('data', function(chunk) {
+        //do something with chunk
+        console.log(chunk);
+      });
+    }).on("error", function(e) {
+      console.log("Got error: " + e.message);
+    });
   }
 }
