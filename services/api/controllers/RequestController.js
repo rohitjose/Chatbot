f70b0835bea03f6448f9307c7ce987f294b32498
Rohit Jose <rohitjose@gmail.com>
2017-10-06 02:54:53 +1100

    FB button hardcoded test

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -23,39 +23,36 @@ module.exports = {
     console.log("################ URL REDIRECT ###################");
 
     // make the GET request
-    request('http://localhost:1337/' + url, function(err, response) {
-      if (err) return console.error(err.message);
+    // request('http://localhost:1337/' + url, function(err, response) {
+    //   if (err) return console.error(err.message);
 
-      console.log(response.body);
-      return res.ok(response.body);
-    });
+    //   console.log(response.body);
+    //   return res.ok(response.body);
+    // });
 
-    // let response = `{
-    //   "speech": "Here you go: ",
-    //   "source": "www.testclan.com",
-    //   "displayText": "Here you go: ",
-    //   "data": {
-    //     "facebook": {
-    //       "attachment": {
-    //         "type": "template",
-    //         "payload": {
-    //           "template_type": "generic",
-    //           "elements": [{
-    //             "title": "Rainbow Six Siege",
-    //             "subtitle": "Blitz Guide",
-    //             "image_url": "http://img.youtube.com/vi/36q5NnL3uSM/0.jpg",
-    //             "buttons": [{
-    //               "type": "web_url",
-    //               "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
-    //               "title": "Watch video"
-    //             }]
-    //           }]
-    //         }
-    //       }
-    //     }
-    //   }
-    // }`;
+    let response = `{
+      "status": "success",
+      "displayText": "Description for COMP9323 e-Enterprise Project :\nCareer: postgraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\nDescription for COMP9323 e-Enterprise Project :\nCareer: undergraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\n",
+      "speech": "Description for COMP9323 e-Enterprise Project :\nCareer: postgraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\nDescription for COMP9323 e-Enterprise Project :\nCareer: undergraduate\nDescription: This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.\n",
+      "source": "http://www.handbook.unsw.edu.au/postgraduate/courses/2017/COMP9323.html",
+      "course": "COMP9323",
+      "data": {
+        "facebook": {
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "generic",
+              "buttons": [{
+                "type": "web_url",
+                "url": "http://www.handbook.unsw.edu.au/postgraduate/courses/2017/COMP9323.html",
+                "title": "COMP9323 Handbook"
+              }]
+            }
+          }
+        }
+      }
+    }`;
 
-    // return res.ok(JSON.parse(response));
+    return res.ok(JSON.parse(response));
   }
 }
