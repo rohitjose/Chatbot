ac84e546de20032ac2e0732f47973bbfc9ca4042
Rohit Jose <rohitjose@gmail.com>
2017-10-06 04:02:50 +1100

    Dynamic template generation test

--- services/api/services/Responses.js
+++ services/api/services/Responses.js
@@ -4,29 +4,31 @@ module.exports = {
   //---------------------------------------------------
 
   course_description: function(courses) {
-    let responseString = '';
+    // let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'Description for '+course.code+' '+course.course_title+':\n';
-      responseString += 'Career: '+course.career+'\n';
-      responseString += 'Description: '+course.description+'\n';
-    });
+    // courses.forEach(function(course, index) {
+    //   responseString += 'Description for ' + course.code + ' ' + course.course_title + ':\n';
+    //   responseString += 'Career: ' + course.career + '\n';
+    //   responseString += 'Description: ' + course.description + '\n';
+    // });
 
-    let response =  {
-      status: 'success',
-      displayText: responseString,
-      speech: responseString,
-      source: courses[0].handbook_link,
-      course: courses[0].code
-    };
-    return addUrlButtonToResponse(response);
+    // let response = {
+    //   status: 'success',
+    //   displayText: responseString,
+    //   speech: responseString,
+    //   source: courses[0].handbook_link,
+    //   course: courses[0].code
+    // };
+    // return addUrlButtonToResponse(response);
+
+    return frameGenericFBTemplate(courses[0]);
   },
 
   course_career: function(courses) {
-    let responseString = 'Career options for:'+courses[0].code+'\n';
+    let responseString = 'Career options for:' + courses[0].code + '\n';
 
-    courses.forEach(function (course, index) {
-      responseString += 'Career: '+course.career+'\n';
+    courses.forEach(function(course, index) {
+      responseString += 'Career: ' + course.career + '\n';
     });
 
     return {
@@ -40,9 +42,9 @@ module.exports = {
   course_enrollment_requirements: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'Enrolment requirements for '+course.code+':\n';
-      responseString += course.enrolment_requirements+'\n';
+    courses.forEach(function(course, index) {
+      responseString += 'Enrolment requirements for ' + course.code + ':\n';
+      responseString += course.enrolment_requirements + '\n';
     });
 
     return {
@@ -53,16 +55,16 @@ module.exports = {
     };
   },
 
-  course_enrollment_capacity: function (courses) {
+  course_enrollment_capacity: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += '\nEnrolment capacity for '+course.code+':\n';
-      course.class_detail.forEach(function (classDetail, index) {
-        responseString += 'Capacity '+classDetail.capacity+'\n';
-        responseString += 'Class# '+classDetail.class_nbr+'\n';
-        responseString += 'Section '+classDetail.section+'\n';
-        responseString += 'Offering period '+classDetail.offering_period+'\n\n';
+    courses.forEach(function(course, index) {
+      responseString += '\nEnrolment capacity for ' + course.code + ':\n';
+      course.class_detail.forEach(function(classDetail, index) {
+        responseString += 'Capacity ' + classDetail.capacity + '\n';
+        responseString += 'Class# ' + classDetail.class_nbr + '\n';
+        responseString += 'Section ' + classDetail.section + '\n';
+        responseString += 'Offering period ' + classDetail.offering_period + '\n\n';
       });
     });
 
@@ -94,12 +96,12 @@ module.exports = {
   course_outline: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'Outline for '+course.code+' can be found here:\n';
+    courses.forEach(function(course, index) {
+      responseString += 'Outline for ' + course.code + ' can be found here:\n';
       //responseString += course.course_outline+'\n';
     });
 
-    let response =  {
+    let response = {
       status: 'success',
       displayText: responseString,
       speech: responseString,
@@ -111,8 +113,8 @@ module.exports = {
   course_page_link: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'You can get details for '+course.code+' at\n';
+    courses.forEach(function(course, index) {
+      responseString += 'You can get details for ' + course.code + ' at\n';
       //responseString += course.handbook_link;
     });
 
@@ -127,8 +129,8 @@ module.exports = {
   course_units_of_credit: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'UOC for '+course.code+': '+course.units_of_credit;
+    courses.forEach(function(course, index) {
+      responseString += 'UOC for ' + course.code + ': ' + course.units_of_credit;
     });
 
     return {
@@ -142,15 +144,15 @@ module.exports = {
   //---------------------------------------------------
   //  ---------- CLASS DETAILS RESPONSES ----------
   //---------------------------------------------------
-  classdetail_day_info: function (courses) {
+  classdetail_day_info: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'Lecture modes for '+course.code+' are:';
-      course.class_detail.forEach(function (classDetail, index) {
-        responseString += '\nClass# '+classDetail.class_nbr+'\n';
-        responseString += 'Section '+classDetail.section+'\n';
-        responseString += 'Instruction mode: '+classDetail.instruction_mode+'\n';
+    courses.forEach(function(course, index) {
+      responseString += 'Lecture modes for ' + course.code + ' are:';
+      course.class_detail.forEach(function(classDetail, index) {
+        responseString += '\nClass# ' + classDetail.class_nbr + '\n';
+        responseString += 'Section ' + classDetail.section + '\n';
+        responseString += 'Instruction mode: ' + classDetail.instruction_mode + '\n';
       });
     });
 
@@ -162,23 +164,23 @@ module.exports = {
     };
   },
 
-  classdetail_instructor: function (courses) {
+  classdetail_instructor: function(courses) {
 
   },
 
-  classdetail_lecture_duration: function (courses) {
+  classdetail_lecture_duration: function(courses) {
 
   },
 
   classdetail_lecture_mode: function(courses) {
     let responseString = '';
 
-    courses.forEach(function (course, index) {
-      responseString += 'Lecture modes for '+course.code+' are:';
-      course.class_detail.forEach(function (classDetail, index) {
-        responseString += '\nClass# '+classDetail.class_nbr+'\n';
-        responseString += 'Section '+classDetail.section+'\n';
-        responseString += 'Instruction mode: '+classDetail.instruction_mode+'\n';
+    courses.forEach(function(course, index) {
+      responseString += 'Lecture modes for ' + course.code + ' are:';
+      course.class_detail.forEach(function(classDetail, index) {
+        responseString += '\nClass# ' + classDetail.class_nbr + '\n';
+        responseString += 'Section ' + classDetail.section + '\n';
+        responseString += 'Instruction mode: ' + classDetail.instruction_mode + '\n';
       });
     });
 
@@ -190,18 +192,18 @@ module.exports = {
     };
   },
 
-  classdetail_lecture_location: function (courses) {
+  classdetail_lecture_location: function(courses) {
 
   },
 
-  classdetail_timetable: function (courses) {
+  classdetail_timetable: function(courses) {
 
   },
 
   //---------------------------------------------------
   //  ---------- EXCEPTION RESPONSES ----------
   //---------------------------------------------------
-  err_response: function(){
+  err_response: function() {
     return {
       status: 'error',
       displayText: 'Sorry! We are having some technical issues right now.\n Try querying after some time'
@@ -218,7 +220,7 @@ module.exports = {
 
 function addUrlButtonToResponse(response) {
   let data = {
-    facebook:{
+    facebook: {
       attachment: {
         type: "template",
         payload: {
@@ -226,7 +228,7 @@ function addUrlButtonToResponse(response) {
           buttons: [{
             type: "web_url",
             url: response.source,
-            title: response.course+' Handbook'
+            title: response.course + ' Handbook'
           }]
         }
       }
@@ -235,4 +237,47 @@ function addUrlButtonToResponse(response) {
   response.data = data;
 
   return response;
+}
+
+function frameGenericFBTemplate(course) {
+  let generic_template = {
+    speech: "Description",
+    source: "chappie_middleware",
+    displayText: "Course Details",
+    data: {
+      facebook: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [{
+              title: `${course.code} ${course.course_title}`,
+              subtitle: course.description,
+              buttons: [{
+                type: "web_url",
+                url: course.handbook_link,
+                title: 'Handbook link'
+              }]
+            }]
+          }
+        }
+      }
+    }
+  };
+
+  if (course.course_outline_link) {
+    generic_template.data.facebook.attachment.payload.elements[0].buttons.push({
+      type: "web_url",
+      url: course.course_outline_link,
+      title: 'Outline link'
+    });
+  }
+
+  if (course.school_link) {
+    generic_template.data.facebook.attachment.payload.elements[0].buttons.push({
+      type: "web_url",
+      url: course.school_link,
+      title: course.school
+    });
+  }
 }
