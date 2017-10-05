764d73838d936a0c9f786d9b8a490e210ec70a2a
Rohit Jose <rohitjose@gmail.com>
2017-10-05 21:56:20 +1100

    Changes for request routing

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -1,21 +1,24 @@
 module.exports = {
-  get: function (req, res) {
+  get: function(req, res) {
 
-    let intent = req.param('intentName');
-    let params = req.param('parameters');
-    let url = 'courseinformation/'+intent+'?parameters='+params;
+    let intent = req.param('intentName') || req.body.result.metadata.intentName;
+    let params = req.param('parameters') || req.body.result.parameters;
+    let url = 'courseinformation/' + intent + '?parameters=' + params;
 
     console.log("################ INCOMING REQUEST ###################");
     console.log(req.body);
     console.log("################ INCOMING REQUEST ###################");
 
     if (!intent || !params) {
+      console.log("################ ERROR IN REQUEST ###################");
       return res.badRequest({
-        status:'error',
-        msg:'invalid arguments'
+        status: 'error',
+        msg: 'invalid arguments'
       });
     }
+    console.log("################ URL REDIRECT ###################");
     console.log(url);
+    console.log("################ URL REDIRECT ###################");
     return res.redirect(url);
   }
 }
