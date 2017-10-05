125233be9ad6cf454842242ebb29b0125aeb3d6b
Rohit Jose <rohitjose@gmail.com>
2017-10-05 21:40:34 +1100

    Logging for incoming request

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -5,6 +5,10 @@ module.exports = {
     let params = req.param('parameters');
     let url = 'courseinformation/'+intent+'?parameters='+params;
 
+    console.log("################ INCOMING REQUEST ###################");
+    console.log(req);
+    console.log("################ INCOMING REQUEST ###################");
+
     if (!intent || !params) {
       return res.badRequest({
         status:'error',
