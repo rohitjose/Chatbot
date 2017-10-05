7e34bfd488d8bee8252d3c852d4e6b65538823cf
Rohit Jose <rohitjose@gmail.com>
2017-10-06 02:30:28 +1100

    Hardcoded response

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -23,11 +23,39 @@ module.exports = {
     console.log("################ URL REDIRECT ###################");
 
     // make the GET request
-    request('http://localhost:1337/' + url, function(err, response) {
-      if (err) return console.error(err.message);
+    // request('http://localhost:1337/' + url, function(err, response) {
+    //   if (err) return console.error(err.message);
 
-      console.log(response.body);
-      return res.ok(response.body);
-    });
+    //   console.log(response.body);
+    //   return res.ok(response.body);
+    // });
+
+    let response = `{
+      "speech": "Here you go: ",
+      "source": "www.testclan.com",
+      "displayText": "Here you go: ",
+      "data": {
+        "facebook": {
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "generic",
+              "elements": [{
+                "title": "Rainbow Six Siege",
+                "subtitle": "Blitz Guide",
+                "image_url": "http://img.youtube.com/vi/36q5NnL3uSM/0.jpg",
+                "buttons": [{
+                  "type": "web_url",
+                  "url": "https://www.youtube.com/watch?v=36q5NnL3uSM",
+                  "title": "Watch video"
+                }]
+              }]
+            }
+          }
+        }
+      }
+    }`;
+
+    return res.ok(JSON.parse(response));
   }
 }
