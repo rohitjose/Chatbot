3b9328658bf4d727e4f95d608b01f7c13ecd8d3b
Rohit Jose <rohitjose@gmail.com>
2017-10-06 20:28:35 +1100

    Hardcoded test for COMP9323 desription

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -26,15 +26,46 @@ module.exports = {
     res.header("Content-Type", "application/json; charset=utf-8");
 
     // make the GET request
-    request('http://localhost:1337/' + url, function(err, response) {
+    // request('http://localhost:1337/' + url, function(err, response) {
 
-      if (err) return console.error(err.message);
+    //   if (err) return console.error(err.message);
 
-      console.log("################ MIDDLEWARE RESPONSE ###################");
-      console.log(response.body);
-      console.log("################ MIDDLEWARE RESPONSE ###################");
+    //   console.log("################ MIDDLEWARE RESPONSE ###################");
+    //   console.log(response.body);
+    //   console.log("################ MIDDLEWARE RESPONSE ###################");
 
-      return res.ok(response.body);
-    });
+    //   return res.ok(response.body);
+    // });
+
+    let response = `{
+  "speech": "Description",
+  "source": "chappie_middleware",
+  "displayText": "Course Details",
+  "data": {
+    "facebook": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "This course covers principles, techniques, architectures, and enabling technologies for the development of the different components and layers of complex e-Commerce systems (presentation and personalisation layer, business logic, message exchange). In particular, it discusses the processes related to (1) building a large enterprise system in collaboration with other project members, (2) developing software in a culture of participation, (3) developing software using agile software engineering methodologies, (4) integrating a number of separate components to build an integrated system, and (5) developing appropriate testing strategies and methodologies for given projects.",
+          "buttons": [{
+            "type": "web_url",
+            "url": "http://www.handbook.unsw.edu.au/postgraduate/courses/2017/COMP9323.html",
+            "title": "Handbook link"
+          }, {
+            "type": "web_url",
+            "url": "http://www.cse.unsw.edu.au/~cs9323",
+            "title": "Outline link"
+          }, {
+            "type": "web_url",
+            "url": "http://www.cse.unsw.edu.au/",
+            "title": "School Page"
+          }]
+        }
+      }
+    }
+  }`;
+
+    return res.ok(JSON.parse(response));
   }
 }
