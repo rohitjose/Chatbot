f5d5508b2844325cb626344e9863f5880edf95bb
Rohit Jose <rohitjose@gmail.com>
2017-10-08 13:34:17 +1100

    Hardcoded test for list

--- services/api/controllers/RequestController.js
+++ services/api/controllers/RequestController.js
@@ -25,48 +25,92 @@ module.exports = {
     // Set the response header
     res.header("Content-Type", "application/json; charset=utf-8");
 
-    // make the GET request
-    request('http://localhost:1337/' + url, function(err, response) {
+    // // make the GET request
+    // request('http://localhost:1337/' + url, function(err, response) {
 
-      if (err) return console.error(err.message);
+    //   if (err) return console.error(err.message);
 
-      console.log("################ MIDDLEWARE RESPONSE ###################");
-      console.log(response.body);
-      console.log("################ MIDDLEWARE RESPONSE ###################");
+    //   console.log("################ MIDDLEWARE RESPONSE ###################");
+    //   console.log(response.body);
+    //   console.log("################ MIDDLEWARE RESPONSE ###################");
 
-      return res.ok(response.body);
-    });
+    //   return res.ok(response.body);
+    // });
 
-//     let response = `{
-//   "speech": "Description",
-//   "source": "chappie_middleware",
-//   "displayText": "Course Details",
-//   "data": {
-//     "facebook": {
-//       "attachment": {
-//         "type": "template",
-//         "payload": {
-//           "template_type": "button",
-//           "text": "Hardcoded response",
-//           "buttons": [{
-//             "type": "web_url",
-//             "url": "http://www.handbook.unsw.edu.au/postgraduate/courses/2017/COMP9323.html",
-//             "title": "Handbook link"
-//           }, {
-//             "type": "web_url",
-//             "url": "http://www.cse.unsw.edu.au/~cs9323",
-//             "title": "Outline link"
-//           }, {
-//             "type": "web_url",
-//             "url": "http://www.cse.unsw.edu.au/",
-//             "title": "School Page"
-//           }]
-//         }
-//       }
-//     }
-//   }
-// }`;
+    let response = `{
+  "speech": "Description",
+  "source": "chappie_middleware",
+  "displayText": "Course Details",
+  "data": {
+    "facebook": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+        "template_type": "list",
+        "top_element_style": "compact",
+        "elements": [
+          {
+            "title": "Classic T-Shirt Collection",
+            "subtitle": "See all our colors",
+            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+            "buttons": [
+              {
+                "title": "View",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+              }
+            ]
+          },
+          {
+            "title": "Classic White T-Shirt",
+            "subtitle": "See all our colors",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+              "messenger_extensions": true,
+              "webview_height_ratio": "tall",
+              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+            }
+          },
+          {
+            "title": "Classic Blue T-Shirt",
+            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+            "subtitle": "100% Cotton, 200% Comfortable",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+              "messenger_extensions": true,
+              "webview_height_ratio": "tall",
+              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+            },
+            "buttons": [
+              {
+                "title": "Shop Now",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+              }
+            ]        
+          }
+        ],
+         "buttons": [
+          {
+            "title": "View More",
+            "type": "postback",
+            "payload": "payload"            
+          }
+        ]  
+      }
+      }
+    }
+  }
+}`;
 
-//     return res.ok(JSON.parse(response));
+    return res.ok(JSON.parse(response));
   }
 }
