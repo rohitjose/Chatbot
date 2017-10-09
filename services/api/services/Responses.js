e2123d37c2b6724b0938962c3acdbb3edac0d69b
Rohit Jose <rohitjose@gmail.com>
2017-10-09 13:03:21 +1100

    Test for generic template use in course lookup

--- services/api/services/Responses.js
+++ services/api/services/Responses.js
@@ -71,18 +71,8 @@ module.exports = {
       }
     }
 
-    // Build payload
-    let main_button_array = [];
 
-    main_button_array.push(defineFBButton("http://www.handbook.unsw.edu.au/2018/index.html", "UNSW Handbook"));
-    let payload = {
-      template_type: "list",
-      top_element_style: "compact",
-      elements: courseList,
-      buttons: main_button_array
-    };
-
-    return frameListFBTemplateFromPayload(payload);
+    return frameGenericFBTemplateFromElements(elements);
 
   },
 
@@ -292,12 +282,12 @@ module.exports = {
       let text = '';
 
       //clashes.forEach(function(clash, index) {
-        text += 'Clash in ' + clashes[0].c1.code + ' and ' + clashes[0].c2.code+'\n';
-        text += clashes[0].c1.code + ' has classes on ' + clashes[0].c1.day + ' from ' + clashes[0].c1.time + ' and ';
-        text += clashes[0].c2.code + ' has classes on the same day from ' + clashes[0].c2.time;
+      text += 'Clash in ' + clashes[0].c1.code + ' and ' + clashes[0].c2.code + '\n';
+      text += clashes[0].c1.code + ' has classes on ' + clashes[0].c1.day + ' from ' + clashes[0].c1.time + ' and ';
+      text += clashes[0].c2.code + ' has classes on the same day from ' + clashes[0].c2.time;
 
-        buttons.push(defineFBButton(clashes[0].c1.course.class_timetable_link, '' + clashes[0].c1.code + ' Timetable'));
-        buttons.push(defineFBButton(clashes[0].c2.course.class_timetable_link, '' + clashes[0].c2.code + ' Timetable'));
+      buttons.push(defineFBButton(clashes[0].c1.course.class_timetable_link, '' + clashes[0].c1.code + ' Timetable'));
+      buttons.push(defineFBButton(clashes[0].c2.course.class_timetable_link, '' + clashes[0].c2.code + ' Timetable'));
       //});
 
       let response = frameButtonFBTemplate(null, text, false, false, false, true);
@@ -541,4 +531,30 @@ function timeOverlap(t1, t2) {
 
   return range1.overlaps(range2);
 
+}
+
+
+// Generates a generic FB template for a multiple elements
+function frameGenericFBTemplateFromElements(elements) {
+  subtitle = (subtitle == "") ? course.description : subtitle;
+  if (!title || title == "") title = course.code + ' ' + course.course_title;
+
+  let generic_template = {
+    speech: "Description",
+    source: "chappie_middleware",
+    displayText: "Course Details",
+    data: {
+      facebook: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: elements
+          }
+        }
+      }
+    }
+  };
+
+  return generic_template;
 }
