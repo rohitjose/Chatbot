e7aeaf310e64260714922abc52ab5cd7080947d4
fahd alhamazani <fahdalhamazani@fahds-MacBook-Pro.local>
2017-10-24 11:29:08 +1100

    add another replacing to clean_attributes func.

--- dataLoad_scripts/course_details.js
+++ dataLoad_scripts/course_details.js
@@ -9,6 +9,7 @@ var connection_handle = {};
 
 // Removed the ':' sign trims the values to
 // form the attribute name value
+//remove tags
 var clean_attribute = function(attribute) {
 	attribute = attribute.toLowerCase();
 	attribute = attribute.replace(/:/g, "");
